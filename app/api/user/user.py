35202b1b9ce19b57782859d62653ed71f3abdc61
전은형 <ooooo5h91@gmail.com>
2022-01-10 21:42:36 +0900

    임시 조건문 제거 -> 실제 DB 쿼리
    DB에서 받아낸 내용 분석 ~ 실제 상황에 맞는 응답 작성 예정

--- app/api/user/user.py
+++ app/api/user/user.py
@@ -1,3 +1,5 @@
+from pymysql import connect
+from pymysql.cursors import DictCursor
 # 사용자 정보 관련된 기능들을 모아두는 모듈
 # app.py에서 이 함수들을 끌어다 사용
 
@@ -12,16 +14,28 @@ def user_test():
     }
     
 def login_test(id, pw):
-    # 아이디 : admin / 비번 : qwer 로그인 성공 응답
-    # 그 외 : 실패처리
+    # id, pw를 이용해서  -> SQL 쿼리 작성 -> 결과에 따라 다른 응답 리턴
     
-    if id == 'admin' and pw == 'qwer':
-        return {
-            'code' : 200,
-            'message' : 'login ok',
-        }, 200 
-    else :
-        return {
-            'code' : 400,
-            'message' : 'id or pw incorrect',
-        }, 400
+    db = connect(
+        host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com',
+        port=3306,
+        user='admin',
+        passwd='Vmfhwprxm!123',
+        db='test_phone_book',
+        charset='utf8',
+        cursorclass=DictCursor
+    )
+    
+    cursor = db.cursor()
+    
+    sql = f"SELECT * FROM users WHERE email='{id}' AND password='{pw}'"
+    cursor.execute(sql)
+    
+    query_result = cursor.fetchone()  # 검색결과 없으면, None 리턴. 검색결과 있으면, 사용자의 정보를 담은 dict 리턴
+    print(query_result)
+    
+    return{
+        'test' : 'test',
+    }
+    
+    
