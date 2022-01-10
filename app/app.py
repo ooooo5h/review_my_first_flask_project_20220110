248fa076dc558e441f7699270b24ac997f7a85dd
전은형 <ooooo5h91@gmail.com>
2022-01-10 21:03:13 +0900

    코드 구조 변경 관련 주석 추가

--- app/app.py
+++ app/app.py
@@ -13,50 +13,10 @@ def create_app():
     def home():
         # return 내용 : HTML등 웹 프론트엔드 태그
         return "<h1>Hello World!</h1>"  # Hello World 문장 리턴 => 이 내용을 사용자에게 보여주겠다.
-    
-    @app.route("/test")  # 서버의 /test주소로 오면 수행해줄 일을 작성
-    def test():
-        return "이곳은 테스트 페이지입니다."
-    
-    @app.route("/web")
-    def web_test():
-        return render_template('web_test.html')  # templates 폴더 내부의 파일을 불러주는 역할
-    
-    @app.route("/json")
-    def json_test():
-        # JSON : 양식 => "이름표" : 실제 값의 조합(딕셔너리를 이용하면 작업이 편하다.)
-        test_dict = {}
-        test_dict['name'] = '전은형'
-        test_dict['birth_year'] = 1991
-        test_dict['height'] = 165.6
-        test_dict['is_male'] = False
-        
-        return jsonify(test_dict) # dict -> json 변경 함수 활용, JSON 응답 내려주기
-    
-    @app.route('/json2')
-    def json_test2():
-        hello_dict = {}
-        hello_dict['korean'] = '안녕하세요'
-        hello_dict['english'] = 'Hello'
-        
-        return hello_dict
-    
-    @app.route('/json3')
-    def json_test3():
-        
-        user_dict = {}
-        user_dict['name'] = 'JEH'
-        user_dict['birth_date'] = '1991-03-14'
-        
-        return {
-            'code' : 200,
-            'message' : 'json test ok',
-            'data' : user_dict,
-        }   
-        
+
     @app.route('/module_test')
     def module_test():
-        return user_test() 
+        return user_test()  # 다른 모듈의 함수의 실행결과를 내보내자 => 로직은 다른 모듈에서만 작성하면 됨
     
     
     # 이 서버를 사용하도록 결과로 내보내기
