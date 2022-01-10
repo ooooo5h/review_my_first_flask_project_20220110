1a6db068dd652097a2d84fc3e20bbcd965c28857
전은형 <ooooo5h91@gmail.com>
2022-01-10 20:39:15 +0900

    주석 추가

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def create_app():
     
     @app.route("/web")
     def web_test():
-        return render_template('web_test.html')
+        return render_template('web_test.html')  # templates 폴더 내부의 파일을 불러주는 역할
     
     
     # 이 서버를 사용하도록 결과로 내보내기
